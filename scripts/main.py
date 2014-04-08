d6254603f0eea5f0003b52fab061b3fb1dcbd483
Haneen Alhassani <haneen1@boston.ischool.uw.edu>
2014-04-07 17:11:58 -0700

    fixed main

--- scripts/main.py
+++ scripts/main.py
@@ -3,9 +3,9 @@ import inspect, basic_graphs
 def main():
         print 'Starbird Mongo Datasets: V 1.0'
         count = 0
-        for x in inspect.getmembers(basic_graphs, inspect.isfunction):
+        for i,x in enumerate(inspect.getmembers(basic_graphs, inspect.isfunction)):
                 if x[0][0] != '_' and x[0] != 'main':
-                        print '[%s] %s' % (count,x[0])
+                        print '[%s] %s' % (i,x[0])
                         count += 1
         option = int(raw_input('>> '))
         inspect.getmembers(basic_graphs, inspect.isfunction)[option][1]()
