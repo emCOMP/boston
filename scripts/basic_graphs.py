e403321cd37002c024fd1e4359189c3a6c339690
Daniel Sandoval <dansand@uw.edu>
2014-04-25 09:42:36 -0700

    Generalized the basic graphs for use by non-programmers. Added GPS retrieval code.

--- scripts/basic_graphs.py
+++ scripts/basic_graphs.py
@@ -1,7 +1,12 @@
 from connection import dbConnection
 from collections import Counter
 from datetime import datetime
+from sys import stdout
+import random
 import utils
+
+rumors = ['girl running','sunil','seals/craft','cell phone','proposal','jfk']
+default_database = "new_boston"
 
 def _rumor_over_time(db_name,rumor,gran,fname):
     db = dbConnection()
@@ -22,7 +27,7 @@ def _rumor_over_time(db_name,rumor,gran,fname):
                 dateStart = datetime(2013,04,i,j,k)
                 dateEnd = datetime(2013,04,i,j,(k+9),59)
                 #print "time: %s,%s" % (dateStart,dateEnd)
-
+                print "querying database..."
                 raw_data = db.m_connections[db_name].find({
                     "created_ts":{
                         "$gte":dateStart,
@@ -58,18 +63,60 @@ def _rumor_over_time(db_name,rumor,gran,fname):
                                                       misinfo,
                                                       correction,
                                                       other)
-
+                print "writing data"
                 f.write(result)
 
-def top_hashtags(db_name):
+def _total_tweets_over_time(db_name,fname):
     db = dbConnection()
     db.create_mongo_connections(mongo_options=[db_name])
 
-    title = "iconf_top_hashtags.csv"
+    title = "%s.csv" % (fname)
+    fpath = utils.write_to_data(path=title)
+    f = open(fpath, 'w')
+    f.write('time,total tweets\n')
+
+    for i in range(15,23): #15-23 (day)
+        for j in range(0,24): #0-24 (hour)
+            for k in range(0,60,10):
+                dateStart = datetime(2013,04,i,j,k)
+                dateEnd = datetime(2013,04,i,j,(k+9),59)
+                #print "time: %s,%s" % (dateStart,dateEnd)
+                print "querying database..."
+                raw_data = db.m_connections[db_name].find({
+                    "created_ts":{
+                        "$gte":dateStart,
+                        "$lte":dateEnd
+                    }
+                }).count()
+                print "writing data"
+                result = '"%s",%d\n' % (dateStart,raw_data)
+                f.write(result)
+
+def rumor_over_time():
+    print '\n*rumor_over_time*\n\nThis method outputs tweet features for all of the data in a selection\n'
+    print 'enter a valid file name:'
+    user_in = raw_input('>> ')
+
+    print 'would you like to data from coded data or all collected data? (0 for coded, 1 for all):'
+    user_in = raw_input('>> ')
+    if int(user_in) is 0:
+        for x in rumors:
+            _rumor_over_time(db_name=default_database,rumor=x,gran=True,fname=user_in)
+    elif int(user_in) is 1:
+        _total_tweets_over_time(db_name='gnip_boston',fname=user_in)
+    else:
+        print "please enter a valid response"
+    print "done!\nexiting..."
+
+def _top_hashtags(db_name, fname, rumor):
+    db = dbConnection()
+    db.create_mongo_connections(mongo_options=[db_name])
+
+    title = "%s_%s.csv" % (rumor.replace('/','_'), fname)
     fpath = utils.write_to_data(path=title)
     f = open(fpath, 'w')
     f.write('name,count\n')
-
+    print "querying database..."
     data = db.m_connections[db_name].find({
         'counts.hashtags':{
             '$gt':0
@@ -77,36 +124,71 @@ def top_hashtags(db_name):
     })
 
     count = Counter()
-
+    print "writing data"
     for x in data:
         count.update(x['hashtags'])
 
     for x in count.most_common(100):
         print x
 
-def top_urls(db_name):
+def top_hashtags():
+    print '\n*top_hashtags*\n\nThis method outputs the 100 most common hashtags in a selection\n'
+    print 'enter a valid file name:'
+    user_in = raw_input('>> ')
+    print 'please select a rumor (enter 0 for all):'
+    i = 1
+    for item in rumors:
+        print '[%i] %s' % (i, item)
+        i += 1
+    user_select = raw_input('>> ')
+    if user_select is 0:
+        for x in rumors:
+            _top_hashtags(db_name=default_database, fname=user_in, rumor=x)
+    else:
+        _top_hashtags(db_name=default_database, fname=user_in, rumor=rumors[int(user_select) - 1])
+    print "done!\nexiting..."
+
+def _top_urls(db_name, fname, rumor):
     db = dbConnection()
     db.create_mongo_connections(mongo_options=[db_name])
 
-    title = "iconf_top_hashtags.csv"
+    title = "%s_%s.csv" % (rumor.replace('/','_'), fname)
     fpath = utils.write_to_data(path=title)
     f = open(fpath, 'w')
     f.write('name,count\n')
-
+    print "querying database..."
     data = db.m_connections[db_name].find({
         'counts.urls':{
             '$gt':0
-        }
+        },
+        'counts.rumor':rumor
     })
 
     count = Counter()
-
+    print "writing data"
     for x in data:
         for y in x['entities']['urls']:
             count.update([y['expanded_url']])
 
     for x in count.most_common(100):
         print x
+
+def top_urls():
+    print '\n*top_urls*\n\nThis method outputs the top 100 most common urls in a selection\n'
+    print 'enter a valid file name:'
+    user_in = raw_input('>> ')
+    print 'please select a rumor (enter 0 for all):'
+    i = 1
+    for item in rumors:
+        print '[%i] %s' % (i, item)
+        i += 1
+    user_select = raw_input('>> ')
+    if user_select is 0:
+        for x in rumors:
+            _top_urls(db_name=default_database, fname=user_in, rumor=x)
+    else:
+        _top_urls(db_name=default_database, fname=user_in, rumor=rumors[int(user_select) - 1])
+    print "done!\nexiting..."
 
 def _text_by_time(db_name,rumor,fname,start_time,end_time,code):
     db = dbConnection()
@@ -116,7 +198,7 @@ def _text_by_time(db_name,rumor,fname,start_time,end_time,code):
     fpath = utils.write_to_data(path=title)
     f = open(fpath, 'w')
     f.write('time,rumor text\n')
-
+    print "querying database..."
     raw_data = db.m_connections[db_name].find({
         "created_ts":{
             "$gte":start_time,
@@ -125,7 +207,7 @@ def _text_by_time(db_name,rumor,fname,start_time,end_time,code):
         "codes.rumor":rumor,
         "codes.code":code
     })
-
+    print "writing data"
     for x in raw_data:
         result = '%s,"%s\n"' % (x['created_at'],x['text'])
         try:
@@ -135,11 +217,17 @@ def _text_by_time(db_name,rumor,fname,start_time,end_time,code):
             f.write(result)
 
 def text_by_time():
+    print '\n*text_by_time*\n\nThis method outputs tweet text in a selected time frame\n'
     print 'enter a valid file name:'
     fname_in = raw_input('>> ')
-    print 'enter a rumor:'
-    rumor_in = raw_input('>> ')
-    print 'enter a code:'
+    print 'please select a rumor (enter 0 for all):'
+    i = 1
+    for item in rumors:
+        print '[%i] %s' % (i, item)
+        i += 1
+    user_select = raw_input('>> ')
+    rumor_in = rumors[int(user_select) - 1]
+    print 'enter a code (misinfo, speculation, hedge, question, correction, other, unrelated):'
     code_in = raw_input('>> ')
     print 'enter a day (15 through 22):'
     day = int(raw_input('>> '))
@@ -150,70 +238,82 @@ def text_by_time():
     dateStart = datetime(2013,04,day,hour,minute)
     dateEnd = datetime(2013,04,day,hour,minute,59)
 
-    _text_by_time(db_name='new_boston',
+    _text_by_time(db_name=default_database,
                   rumor=rumor_in,
                   fname=fname_in,
                   start_time=dateStart,
                   end_time=dateEnd,
                   code=code_in)
+    print "done!\nexiting..."
 
-def rumor_over_time():
-    rumors = ['girl running','sunil','seals/craft','cell phone','proposal','jfk']
+def _gps_over_time(db_name, fname, rumor="all"):
+    db = dbConnection()
+    db.create_mongo_connections(mongo_options=[db_name])
 
+    title = "%s_%s.csv" % (rumor.replace('/','_'), fname)
+    fpath = utils.write_to_data(path=title)
+    f = open(fpath, 'w')
+    print "querying database..."
+    if rumor is "all":
+        raw_data = db.m_connections[db_name].find({
+        'place.coordinates.type':
+            'Point'
+        })
+    else:
+        raw_data = db.m_connections[db_name].find({
+        'place.coordinates.type':
+            'Point',
+            "codes.rumor":rumor
+        })
+    f.write('title,url,text,author,time,code,lat,lon\n')
+    print "writing data"
+    for (i,data) in enumerate(raw_data):
+    	try:
+    		title = data['entities']['urls'][0]['title']
+    	except:
+    		title = ""
+    	try:
+    		url = data['entities']['urls'][0]['long-url']
+    	except:
+    		url = ""
+        try:
+            code = data['codes'][0]['code']
+        except:
+            code = ""
+    	text = data['text']
+    	author = data['user']['id']
+    	time = data['created_at']
+    	try:
+    		lat = data['place']['coordinates']['coordinates'][0]
+    	except:
+    		lat = ""
+    	try:
+    		lon = data['place']['coordinates']['coordinates'][1]
+    	except:
+    		lon = ""
+    	# print i ...for testing
+    	result = '"%s","%s","%s","%s","%s","%s","%s","%s"\n' % (title,url,text,author,time,code,lat,lon)
+    	# print result ... for testing
+    	try:
+    		f.write(result)
+    	except:
+    		f.write('decode error!\n')
+
+def gps_over_time():
+    print '\n*gps_over_time*\n\nThis method outputs features for all tweets with gps coordinates in a selection\n'
     print 'enter a valid file name:'
     user_in = raw_input('>> ')
-
-    for x in rumors:
-        _rumor_over_time(db_name='new_boston',rumor=x,gran=True,fname=user_in)
-
-# retrieves relevant data for all documents with GPS data
-def _getAllGPS(db_name, fname, rumor="all"):
-	db = dbConnection()
-	db.create_mongo_connections(mongo_options=[db_name])
-	
-	title = "%s_%s.csv" % (rumor.replace('/','_'), fname)
-	fpath = utils.write_to_data(path=title)
-	f = open(fpath, 'w')
-	
-	raw_data = db.m_connections[db_name].find({'place.coordinates.type':'Point'})
-	f.write('title,url,text,author,time,lat,lon\n')
-	for (i,data) in enumerate(raw_data):
-		try:
-			title = data['entities']['urls'][0]['title']
-		except:
-			title = ""
-		try:
-			url = data['entities']['urls'][0]['long-url']
-		except:
-			url = ""
-		text = data['text']
-		author = data['user']['id']
-		time = data['created_at']
-		try:
-			lat = data['place']['coordinates']['coordinates'][0]
-		except:
-			lat = ""
-		try:
-			lon = data['place']['coordinates']['coordinates'][1]
-		except:
-			lon = ""
-		# print i ...for testing
-		result = '"%s","%s","%s","%s","%s","%s","%s"\n' % (title,url,text,author,time, lat, lon)
-		# print result ... for testing
-		try:
-			f.write(result)
-		except:
-			f.write('decode error!\n')
-
-def getAllGPS():
-	rumors = ['girl running','sunil','seals/craft','cell phone','proposal','jfk'] #to make separate csv's (not yet implemented)
-	print 'enter a valid file name:'
-	user_in = raw_input('>> ')
-	
-	_getAllGPS(db_name='new_boston', fname=user_in)
-
-def main():
-    getAllGPS()
+    print 'please select a rumor (enter 0 for all):'
+    i = 1
+    for item in rumors:
+        print '[%i] %s' % (i, item)
+        i += 1
+    user_select = raw_input('>> ')
+    if int(user_select) is 0:
+        _gps_over_time(db_name=default_database, fname=user_in)
+    else:
+        _gps_over_time(db_name=default_database, fname=user_in, rumor=rumors[int(user_select) - 1])
+    print "done!\nexiting..."
 
 if __name__ == "__main__":
-    main()
+    print "please use main.py to access this program"
--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,15 @@
 import inspect, basic_graphs
+correction = 5
 
 def main():
-        print 'Starbird Mongo Datasets: V 1.0'
-        for (i,x) in enumerate(inspect.getmembers(basic_graphs, inspect.isfunction)):
-                if x[0][0] != '_':
-                        print '[%s] %s' % (i,x[0])
-        option = int(raw_input('>> '))
-        inspect.getmembers(basic_graphs, inspect.isfunction)[option][1]()
+	print 'Starbird Mongo Datasets: V 1.0'
+	print 'please select an option (0 to exit):'
+	for (i,x) in enumerate(inspect.getmembers(basic_graphs, inspect.isfunction)):
+		if x[0][0] != '_':
+			print '[%s] %s' % (i - correction,x[0])
+	option = int(raw_input('>> ')) + correction
+	if option is not correction:
+		inspect.getmembers(basic_graphs, inspect.isfunction)[option][1]()
 
 if __name__ == "__main__":
         main()
